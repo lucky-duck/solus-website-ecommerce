916f18b8658ecc387f0e9026cb2284d9fe172d5b
Andrey Nikonov <a.einomi@gmail.com>
2019-11-08 19:28:46 +0300

    Add astroturf

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -7,5 +7,17 @@ module.exports = (config) => {
   };
   config.output.filename = 'static/js/[name].js';
   config.plugins[5].options.moduleFilename = () => 'static/css/main.css';
+
+  // astroturf
+  config.module.rules.push({
+    test: /\.(js|mjs|jsx|ts|tsx)$/,
+    use: [
+      {
+        loader: 'astroturf/loader',
+        options: { extension: '.module.scss' },
+      },
+    ],
+  });
+
   return config;
 };
--- src/screens/product.jsx
+++ src/screens/product.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'astroturf';
+
+import productImage from '../images/product.jpg';
+
+const Flex = styled('div')`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Image = styled('img')``;
+
+const Content = styled('div')``;
+
+const Text = styled('div')`
+  font-weight: 700;
+`;
 
 function ProductScreen() {
-  return <h1>Test</h1>;
+  return (
+    <Flex>
+      <Image src={productImage} alt={'Product'} />
+      <Content>
+        <Text>Select your SOLUS+</Text>
+      </Content>
+    </Flex>
+  );
 }
 
 export default ProductScreen;
