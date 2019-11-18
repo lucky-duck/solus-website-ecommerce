d4f80caa171c672be396c5b68e173960655af1b3
Andrey Nikonov <a.einomi@gmail.com>
2019-11-18 17:03:20 +0300

    Add promo codes

--- src/constants.js
+++ src/constants.js
@@ -58,3 +58,12 @@ export const PAYPAL_CLIENT_ID_PRODUCTION =
 
 export const PAYPAL_CLIENT_ID_SANDBOX =
   'Ab115IoTrFjgeoyb2z9JEL2Njm7ovP8uBUY-FBWYvQvZMUuIkMb6G-JyIR0DTDw4Xh8bQ8tsaQxVpa8A';
+
+export const PROMO_CODES = {
+  '4b1de0f4': 5,
+  '6d3e6073': 10,
+  '2c7156c0': 15,
+  '7e872d32': 20,
+  '5b1b9d5e': 25,
+  '9f914a24': 30,
+};
