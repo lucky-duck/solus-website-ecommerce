38f57b83679ad6f3bb9e82cac00d8cee02ffe100
Andrey Nikonov <a.einomi@gmail.com>
2019-11-20 13:03:14 +0300

    Fix fonts

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -8,9 +8,10 @@ const mixins = css`
   }
 
   .fontFamilySansAlt {
-    font-family: 'San Francisco Display', -apple-system, BlinkMacSystemFont,
-      'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
-      'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-family: FuturaPTBook, 'Futura PT', 'San Francisco Display',
+      -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
     letter-spacing: -0.02em;
   }
 
