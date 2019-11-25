d5c40c4db1c6499121d72c3d56c39230003f041a
Andrey Nikonov <a.einomi@gmail.com>
2019-11-25 15:05:16 +0300

    Add changes to PayPal buttons

--- src/screens/cart/index.jsx
+++ src/screens/cart/index.jsx
@@ -232,6 +232,10 @@ function Inner({
     removeNodeChildren(paypalButtonContainerNode.current);
 
     paypalButtonsComponent.current = window.paypal.Buttons({
+      style: {
+        layout: 'vertical',
+        label: 'pay'
+      },
       createOrder: function(data, actions) {
         // This function sets up the details of the transaction, including the amount and line item details.
         return actions.order.create({
